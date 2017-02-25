aebc00a26aabc3c1b0574e688ccc58f77b718523
Py64 <py64.wolflinux@gmail.com>
2017-02-25 19:31:29 +0100

    steamcmd dockerfile

---
+++ update-steamcmd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Deleting existing SteamCMD files..."
+rm -rf /data/steamcmd
+
+echo "Downloading the tarball..."
+wget http://media.steampowered.com/client/steamcmd_linux.tar.gz -O /tmp/steamcmd.tar.gz
+
+echo "Extracting..."
+mkdir -p /data/steamcmd
+cd /data/steamcmd
+tar zxf /tmp/steamcmd.tar.gz
+
+echo "Starting user script..."
+bash run.sh
